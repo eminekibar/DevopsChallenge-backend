7e84c60e0de8044a5aa5f9369ca8e49848a43af4
eminekibar <eminekibar.2834@gmail.com>
2025-10-22 21:53:41 +0300

    .

--- Program.cs
+++ Program.cs
@@ -3,13 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// ✅ health checks
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-// Swagger sadece dev'de kalsın
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,7 +15,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 
-// ✅ health endpoint (probe'lar buna bakacak)
+// health endpoint (probe'lar buna bakacak)
 app.MapHealthChecks("/healthz");
 
 app.MapControllers();
