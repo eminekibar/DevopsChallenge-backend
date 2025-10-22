fcb0f5f3d7d71c28d86293de30aa1ce63ff5cd79
eminekibar <eminekibar.2834@gmail.com>
2025-10-22 20:57:29 +0300

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,6 @@ namespace backend.Controllers
         {
             _config = config;
         }
-        //https://localhost:7081/api/values?YourName=Emine
 
         [HttpGet]
         public IActionResult Get(string YourName)
