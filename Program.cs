0fc3cbc091b8f0b946df3a05af3c9acd9e5034c4
eminekibar <eminekibar.2834@gmail.com>
2025-10-23 04:34:58 +0300

    .

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// PostgreSQL bağlantı bilgilerini environment variable'lardan al
+var postgresHost = "postgres";
+var postgresPort = 5432;
+var postgresDb = Environment.GetEnvironmentVariable("POSTGRES_DB");
+var postgresUser = Environment.GetEnvironmentVariable("POSTGRES_USER");
+var postgresPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+
+var connectionString = $"Host={postgresHost};Port={postgresPort};Database={postgresDb};Username={postgresUser};Password={postgresPassword}";
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
+
+// PostgreSQL bağlantısını ekle
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
